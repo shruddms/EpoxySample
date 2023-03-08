0d6968750558e6364b3204f50ce1bef57369b3c6
KyungEun No <74607521+shruddms@users.noreply.github.com>
2023-03-08 23:50:23 +0900

    feat: add item

---
+++ app/src/main/java/com/kyungeun/epoxysample/feature/main/MainController.kt
@@ -0,0 +1,34 @@
+package com.kyungeun.epoxysample.feature.main
+
+import com.airbnb.epoxy.EpoxyController
+import com.kyungeun.epoxysample.data.model.Project
+
+class MainController  : EpoxyController() {
+
+    private val itemList = mutableListOf<Project>()
+
+
+    fun setData(list: List<Project>) {
+        itemList.clear()
+        itemList.addAll(list)
+        requestModelBuild()
+    }
+
+    private fun buildItem(project: Project) {
+        project.let {
+            MainItemViewModel_()
+                .name(it.name)
+                .addTo(this)
+        }
+    }
+
+
+    override fun buildModels() {
+        itemList.forEach { data ->
+            buildItem(data)
+        }
+    }
+}
+
+
+
