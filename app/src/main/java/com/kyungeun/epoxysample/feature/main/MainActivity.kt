fdf1d3722e61696f650d4d9e90ef0ac465a55b59
KyungEun No <74607521+shruddms@users.noreply.github.com>
2023-03-11 21:24:22 +0900

    feat: add item

---
+++ app/src/main/java/com/kyungeun/epoxysample/feature/main/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.kyungeun.epoxysample.feature.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.kyungeun.epoxysample.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    private val viewModel: MainViewModel by viewModels()
+
+    private val mainController = MainController()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupEpoxyRecyclerView()
+        setupObservers()
+    }
+
+    private fun setupEpoxyRecyclerView() {
+        binding.rvHome.layoutManager = LinearLayoutManager(this)
+
+        binding.rvHome.setController(mainController)
+    }
+
+    private fun setupObservers() {
+        viewModel.userProjects.observe(this) {
+//            mainController.setData(it)
+        }
+    }
+}
